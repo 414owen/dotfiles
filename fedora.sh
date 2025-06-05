5834bb164ddb9b352c9e0797426452966debd4a2
Owen Shepherd <owen@owen.cafe>
2025-06-05 15:44:21 +0100

    Remove wild

--- fedora.sh
+++ fedora.sh
@@ -2,8 +2,6 @@
 
 sudo dnf copr enable atim/starship
 sudo dnf install $(command cat packages)
-# Install wild linker:
-cargo install --locked --bin wild --git https://github.com/davidlattimore/wild.git wild
 
 echo "Installing rpmfusion"
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
